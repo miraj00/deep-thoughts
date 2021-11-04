72baff552576f613e8deacf988f11f0df39c4d96
miraj <miraj00@gmail.com>
2021-11-04 06:45:20 -0400

    Front-End User authentication added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,28 +1,93 @@
+// import React from 'react';
+// import ThoughtList from '../components/ThoughtList';
+// import FriendList from '../components/FriendList';
+
+// // import { QUERY_THOUGHTS } from '../utils/queries';
+// import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+
+// // Import the useQuery Hook from Apollo Client. This will allow us to make requests to the GraphQL server we connected to and made available to the application using the <ApolloProvider> component in App.js 
+// import { useQuery } from '@apollo/client';
+
+// // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
+// const { data: userData } = useQuery(QUERY_ME_BASIC);
+
+// import Auth from '../utils/auth';
+
+// const Home = () => {
+//     // use useQuery hook to make query request
+//     const { loading, data } = useQuery(QUERY_THOUGHTS);
+//     const thoughts = data?.thoughts || [];
+//     console.log(thoughts);
+
+//     const loggedIn = Auth.loggedIn();
+
+//   return (
+//     <main>
+//        <div className='flex-row justify-space-between'>
+
+//           <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+//              {loading ? (
+//                 <div>Loading...</div>
+//               ) : (
+//                <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+//               )}
+//           </div>
+
+//           {loggedIn && userData ? (
+//               <div className="col-12 col-lg-3 mb-3">
+//                 <FriendList
+//                   username={userData.me.username}
+//                   friendCount={userData.me.friendCount}
+//                   friends={userData.me.friends}
+//                 />
+//               </div>
+//             ) : null}
+
+//        </div>
+//     </main>
+//   );
+// };
+
+// export default Home;
+
+
 import React from 'react';
 import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
 
-// Import the useQuery Hook from Apollo Client. This will allow us to make requests to the GraphQL server we connected to and made available to the application using the <ApolloProvider> component in App.js 
+import Auth from '../utils/auth';
+// import { useQuery } from '@apollo/react-hooks';
 import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 
 const Home = () => {
-    // use useQuery hook to make query request
-    const { loading, data } = useQuery(QUERY_THOUGHTS);
-    const thoughts = data?.thoughts || [];
-    console.log(thoughts);
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+  const thoughts = data?.thoughts || [];
+
+  const loggedIn = Auth.loggedIn();
 
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>
-      
+      <div className="flex-row justify-space-between">
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
-          <div>Loading...</div>
-            ) : (
+            <div>Loading...</div>
+          ) : (
             <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-            )}
-          </div>         
-            
+          )}
+        </div>
+
+        {loggedIn && userData ? (
+          <div className="col-12 col-lg-3 mb-3">
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
+          </div>
+        ) : null}
+
       </div>
     </main>
   );
